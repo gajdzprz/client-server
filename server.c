448ea6e1c353f8884d4b803911f4981a703945a4
Przemyslaw Gajdzinski <przemyslaw.gajdzinski@tieto.com>
2017-07-11 10:31:59 +0200

    basic server

---
+++ server.c
@@ -0,0 +1,70 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 8081
+
+int main(int argc,char * argv[])
+{
+	int server_socket, client_socket;
+	struct sockaddr_in server_addr, client_addr;
+	int backlog = 1;
+	int size;
+	int yes = 1;
+
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket == -1)
+	{
+		perror("socket error");
+		exit(1);
+	}
+
+	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+	{
+		perror("setsockopt error");
+		exit(1);
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	//server_addr.sin_addr.s_addr = INADDR_ANY;
+	//printf("%d",server_addr.sin_addr.s_addr);
+	server_addr.sin_addr.s_addr = inet_addr("10.0.2.15");
+	memset(&(server_addr.sin_zero), '\0', 8);
+
+	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
+	{
+		perror("bind error");
+		exit(1);
+	}
+
+	if (listen(server_socket, backlog) == -1)
+	{
+		perror("listen error");
+		exit(1);
+	}
+
+	size = sizeof(struct sockaddr_in);
+	client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &size);
+	if (client_socket == -1)
+	{
+		perror("accept error");
+		exit(1);
+	}
+
+	if (send(client_socket, "Test\n", strlen("Test\n"), 0) == -1)
+	{
+		perror("send error");
+		exit(1);
+	}
+
+	close(client_socket);
+
+	return 0;
+}
