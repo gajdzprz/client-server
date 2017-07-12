6ffd0c9f615065614d43c2f1790e9cfae38390f1
Przemyslaw Gajdzinski <przemyslaw.gajdzinski@tieto.com>
2017-07-12 10:26:57 +0200

    Implemented basic login

--- client.c
+++ client.c
@@ -13,17 +13,18 @@
 
 int main(int argc, char *argv[])
 {
-
 	int client_socket, numbytes;
 	char buf[MAXDATASIZE];
 	struct hostent *host;
 	struct sockaddr_in their_addr;
+
 	if (argc != 2)
 	{
 		fprintf(stderr,"usage: client hostname\n");
 		exit(1);
 	}
-	if ((host=gethostbyname(argv[1])) == NULL) 
+
+	if ((host = gethostbyname(argv[1])) == NULL) 
 	{
 		perror("gethostbyname");
 		exit(1);
@@ -34,22 +35,44 @@ int main(int argc, char *argv[])
 		perror("socket");
 		exit(1);
 	}
+
 	their_addr.sin_family = AF_INET;
 	their_addr.sin_port = htons(PORT);
 	their_addr.sin_addr = *((struct in_addr *)host->h_addr);
 	memset(&(their_addr.sin_zero), '\0', 8);
+	
 	if (connect(client_socket, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1) 
 	{
 		perror("connect");
 		exit(1);
 	}
-	if ((numbytes=recv(client_socket, buf, MAXDATASIZE-1, 0)) == -1)
+
+	printf("Podaj login:");
+	scanf("%s",buf);
+	send(client_socket, buf, strlen(buf), 0);
+	memset(buf,'\0', sizeof(buf));
+
+	printf("Podaj haslo:");
+	scanf("%s",buf);
+	send(client_socket, buf, strlen(buf), 0);
+	memset(buf,'\0', sizeof(buf));
+
+	numbytes = recv(client_socket,buf,MAXDATASIZE-1,0);
+	if (numbytes == -1)
 	{
-		perror("recv");
+		perror("Sign in error");
 		exit(1);
 	}
-	buf[numbytes] = '\0';
-	printf("Received: %s",buf);
+	else if (numbytes == 0)
+	{
+		printf("Lost connection, server disconnected\n");
+	}
+	else
+	{
+		printf("%s",buf);
+	}
+
 	close(client_socket);
+
 	return 0 ;
 }
