5234e74e76fd63923dbf769c4cad7a4dae2cb925
Przemyslaw Gajdzinski <przemyslaw.gajdzinski@tieto.com>
2017-07-17 12:43:19 +0200

    Moved loggin into function

--- client.c
+++ client.c
@@ -14,13 +14,14 @@
 #define PORT 8081
 #define MAXDATASIZE 100
 
+void logging(char * json_request, char * json_response, int client_socket);
 void display_menu(void);
 void display_files(char * json);
 
 int main(int argc, char *argv[])
 {
 	int client_socket, numbytes;
-	char buf[MAXDATASIZE], json_request[MAXDATASIZE], json_response[MAXDATASIZE];
+	char json_request[MAXDATASIZE], json_response[MAXDATASIZE];
 	struct hostent *host;
 	struct sockaddr_in their_addr;
 
@@ -53,6 +54,46 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	logging(json_request, json_response, client_socket);
+
+	memset(json_request, '\0', sizeof(json_request));
+	int choice;
+	display_menu();
+	scanf("%d", &choice);
+	strcat(json_request,"{\"choice\": \"");
+	char choice_char[2];
+	sprintf(choice_char, "%d", choice);
+	strcat(json_request,choice_char);
+	strcat(json_request,"\"}");
+
+	if (choice > 0 && choice < 4)
+	{
+		send(client_socket, json_request, strlen(json_request),0);
+		printf("send:%s\n",json_request);
+		memset(json_request,'\0',sizeof(json_request));
+		memset(json_response, '\0', sizeof(json_response));
+		int numb = recv(client_socket, json_response, 99, 0);
+		display_files(json_response);
+		// TO DO: next if statement with choice of which file 
+		// want to download or remove
+	}
+	else if (choice == 4)
+	{
+		printf("Write path to the file:\n");
+	}
+	else
+	{
+		printf("Wrong number!\n");
+	}
+
+	close(client_socket);
+
+	return 0 ;
+}
+
+void logging(char * json_request, char * json_response, int client_socket)
+{
+	char buf[MAXDATASIZE];
 	// creating json login
 	memset(json_request, '\0', sizeof(json_request));
 	strcat(json_request, "{\"login\": \"");
@@ -61,7 +102,6 @@ int main(int argc, char *argv[])
 	scanf("%s",buf);
 
 	strcat(json_request,buf);
-
 	memset(buf,'\0', sizeof(buf));
 
 	printf("Podaj haslo:");
@@ -69,7 +109,6 @@ int main(int argc, char *argv[])
 
 	strcat(json_request,"\", \"password\": \"");
 	strcat(json_request,buf);
-
 	strcat(json_request,"\"}");
 
 	printf("send:%s\n",json_request);
@@ -78,7 +117,7 @@ int main(int argc, char *argv[])
 	memset(buf,'\0', sizeof(buf));
 	memset(json_request,'\0', sizeof(json_request));
 
-	numbytes = recv(client_socket,json_response,MAXDATASIZE-1,0);
+	int numbytes = recv(client_socket,json_response,MAXDATASIZE-1,0);
 	if (numbytes == -1)
 	{
 		perror("Sign in error");
@@ -116,38 +155,6 @@ int main(int argc, char *argv[])
 		}
 	}
 	memset(json_response, '\0', sizeof(json_request));
-
-	int choice;
-	display_menu();
-	scanf("%d", &choice);
-	strcat(json_request,"{\"choice\": \"");
-	char choice_char[1];
-	sprintf(choice_char, "%d", choice);
-	strcat(json_request,choice_char);
-	strcat(json_request,"\"}");
-
-	if (choice > 0 && choice < 4)
-	{
-		send(client_socket, json_request, strlen(json_request),0);
-		printf("send:%s\n",json_request);
-		memset(json_request,'\0',sizeof(json_request));
-		int numb = recv(client_socket, json_response, 99, 0);
-		display_files(json_response);
-		// TO DO: next if statement with choice of which file 
-		// want to download or remove
-	}
-	else if (choice == 4)
-	{
-		printf("Write path to the file:\n");
-	}
-	else
-	{
-		printf("Wrong number!\n");
-	}
-
-	close(client_socket);
-
-	return 0 ;
 }
 
 void display_menu(void)
